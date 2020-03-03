0563985266c0f9fa1cf475d4bbc71a38b710d108
anant1996 <56394549+anant1996@users.noreply.github.com>
2020-03-03 10:46:50 +0530

    test

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/**
+ * @description - This file  is used to write router  and render the parent component to html root
+ * @author -  Anant Sabale
+ */
+// import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render("Hello", document.getElementById('root'));
