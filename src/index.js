c251e6c1a237674fa22ed6febc42e5d4e2c51a7c
anant1996 <56394549+anant1996@users.noreply.github.com>
2020-03-03 15:51:01 +0530

    Test app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@
  * @description - This file  is used to write router  and render the parent component to html root
  * @author -  Anant Sabale
  */
-// import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-ReactDOM.render("Hello", document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
